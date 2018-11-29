34071b7ac721a2e4521ed3b6ef4ef1b1cdf7cddc
Henry Qin <hq6@cs.stanford.edu>
2018-11-29 16:35:49 -0700

    Added trial count and timing

--- Benchmark.cc
+++ Benchmark.cc
@@ -1,18 +1,46 @@
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 #include "PerfUtils/TimeTrace.h"
 #include "PerfUtils/Cycles.h"
 
 #include "TestObj.pb.h"
 
-int main(){
+#define NUM_TRIALS 1000000
+
+using PerfUtils::Cycles;
+
+/**
+  * Pass in a the number of 4-byte integers to add to the message.
+  */
+int main(int argc, char** argv){
+    int count = 1;
+    if (argc > 1) {
+        count = atoi(argv[1]);
+    }
+
+    srand(time(NULL));
+
     TestMessage msg;
+
     // Add various numbers of items
-    msg.mutable_item()->Add(5);
+    for (int i = 0; i < count; i++) {
+        msg.mutable_item()->Add(rand());
+    }
 
     // Benchmark serialization
     std::string output;
-    msg.SerializeToString(&output);
+    uint64_t startTime = Cycles::rdtsc();
+    for (int i = 0; i < NUM_TRIALS; i++) {
+        bool success = msg.SerializeToString(&output);
+        if (!success) {
+            fprintf(stderr, "Serialization failed\n");
+        }
+    }
+    uint64_t endTime = Cycles::rdtsc();
+    printf("%lu ns\n", Cycles::toNanoseconds(endTime - startTime) / NUM_TRIALS);
 
     google::protobuf::ShutdownProtobufLibrary();
 }
